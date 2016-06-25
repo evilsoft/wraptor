c5a7516730b5f5363605f65f3d0e8f2e95f7f84e
Ian Hofmann-Hicks (evil) <evilsoft@aol.com>
2016-06-25 16:09:19 -0700

    add auto-partial and accept curried functions

--- wraptor.js
+++ wraptor.js
@@ -23,19 +23,21 @@
     }
   }
 
+  function partial(fn, args) {
+    return Function.bind.apply(fn, [null].concat(args))
+  }
+
   return function wraptor(fn) {
     if(!(isFunction(fn))) {
       throw new TypeError('Must provide a function to wrap')
     }
 
-    const safeFn  = fn.bind(null)
+    const safeFn      = fn.bind(null)
+    const argsLength  = safeFn.length
+    const partialFunc = partial.bind(null, ObservableFunction)
+
     const subs    = []
     const done    = false
-
-    function unsubscribe(o) {
-      const idx = subs.indexOf(o)
-      if(idx !== -1) { subs.splice(idx, 1) }
-    }
 
     function subscribe(o) {
       if(!(o && isFunction(o.next))) {
@@ -45,21 +47,33 @@
       if(subs.indexOf(o) === -1) { subs.push(o) }
 
       return {
-        closed,
-        unsubscribe: unsubscribe.bind(null, o)
+        closed: () => done,
+        unsubscribe()  {
+          const idx = subs.indexOf(o)
+          if(idx !== -1) { subs.splice(idx, 1) }
+        }
       }
     }
 
     function ObservableFunction() {
-      if(subs.length) {
-        const args = slice.call(arguments)
+      const args = slice.call(arguments)
 
-        try {
-          const _val = safeFn.apply(null, args)
-          subs.forEach(notifyNext.bind(null, _val))
-        } catch(e) {
-          subs.forEach(notifyError.bind(null, e))
+      if(args.length < argsLength) {
+        return partialFunc(args)
+      }
+
+      try {
+        const _val = safeFn.apply(null, args)
+
+        if(isFunction(_val)) {
+          return partialFunc(args)
         }
+
+        subs.forEach(notifyNext.bind(null, _val))
+
+        return _val
+      } catch(e) {
+        subs.forEach(notifyError.bind(null, e))
       }
     }
 
