1230a3d65bf68a0f11dad53e149bd44b67703131
Ian Hofmann-Hicks (evil) <evilsoft@aol.com>
2016-06-25 23:08:03 -0700

    add some comments and more :lipstick:

--- wraptor.js
+++ wraptor.js
@@ -27,27 +27,41 @@
     return Function.bind.apply(fn, [null].concat(args))
   }
 
+  function applyCurry(fn, arg) {
+    if(!isFunction(fn)) { return fn }
+    return fn.length > 1 ? fn.bind(null, arg) : fn.call(null, arg)
+  }
+
+  // Main entry point for the lib.
   return function wraptor(fn) {
+    // We need a function for this to work.
     if(!(isFunction(fn))) {
       throw new TypeError('Must provide a function to wrap')
     }
+    // remove any binding and only access function once
+    const safeFn = fn.bind(null)
 
-    const safeFn      = fn.bind(null)
+    // cache number of arguments for partial detection
     const argsLength  = safeFn.length
+
+    // partially apply ObserverFunction for returning partials
     const partialFunc = partial.bind(null, ObservableFunction)
 
-    const subs    = []
-    const done    = false
+    // cached list of subscribers
+    const subs  = []
 
     function subscribe(o) {
+      // Bare min is a next function on the provided Observer
       if(!(o && isFunction(o.next))) {
         throw new TypeError('Invalid Observer passed to subscribe')
       }
 
+      // Add the Observer to the list of subscribers
       if(subs.indexOf(o) === -1) { subs.push(o) }
 
+      // Return a Subcription object
       return {
-        closed: () => done,
+        closed() { return false },
         unsubscribe()  {
           const idx = subs.indexOf(o)
           if(idx !== -1) { subs.splice(idx, 1) }
@@ -56,31 +70,41 @@
     }
 
     function ObservableFunction() {
+      // arraylike args to Array
       const args = slice.call(arguments)
 
+      // check for partial application
+      // return a partially applied Observer Function
       if(args.length < argsLength) {
         return partialFunc(args)
       }
 
       try {
-        const _val = safeFn.apply(null, args)
+        const _val = (args.length === argsLength)
+          ? safeFn.apply(null, args)          // Fully applied, run function
+          : args.reduce(applyCurry, safeFn)   // Possible curry, fold arguments
 
-        if(isFunction(_val)) {
-          return partialFunc(args)
-        }
+        // If we need more arguments, return partial ObserverFunction
+        if(isFunction(_val)) { return partialFunc(args) }
 
+        // Notify all subscribers with result, only if we are fully applied
         subs.forEach(notifyNext.bind(null, _val))
 
+        // return the final value for fully applied function
         return _val
       } catch(e) {
-        subs.forEach(notifyError.bind(null, e))
+        // Notify subscribers on error channel,
+        // or rethrow if we do not have anyone listening
+        if(subs.length) {
+          subs.forEach(notifyError.bind(null, e))
+        } else { throw err }
       }
     }
 
-    ObservableFunction[Symbol.observable] = () => {
-      return { subscribe }
-    }
+    // Provide subscribe interface on Symbol.observable
+    ObservableFunction[Symbol.observable] = () => ({ subscribe })
 
+    // Provide subscribe method on function
     ObservableFunction.subscribe = subscribe
 
     return ObservableFunction
